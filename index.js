b83c2e081f5018cc1980a0c145d3f27fde660bb6
Neamul01 <neamulhaque2002@gmail.com>
2022-05-02 18:59:56 +0600

    api protected with problem that i dont understand

--- index.js
+++ index.js
@@ -12,6 +12,23 @@ app.use(cors());
 app.use(express.json());
 
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unautorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        console.log('decoded', decoded)
+        req.decoded = decoded;
+        next();
+    })
+    // console.log('inside', authHeader);
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bo3cb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -63,12 +80,18 @@ async function run() {
 
         //order collection api
 
-        app.get('/order', async (req, res) => {
+        app.get('/order', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            const query = { email: email };
-            const cursor = orderCollection.find(query);
-            const order = await cursor.toArray();
-            res.send(order)
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = orderCollection.find(query);
+                const order = await cursor.toArray();
+                res.send(order)
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden access' })
+            }
         })
 
         app.post('/order', async (req, res) => {
